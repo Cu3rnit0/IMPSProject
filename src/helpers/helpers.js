e5edf4445d3cfa8e742f1b9f3761a7615920d909
Calzadilla <cg19i04001@usonsonate.edu.sv>
2024-11-01 16:04:53 -0600

    implement_helpers

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,1 +1,25 @@
-const helpers 
+const helpers = require('handlerbars');
+const moment = require('moment');
+
+helpers.registerHelper('ifCond', function(v1, operator, v2, options){
+    switch(operator){
+        case'==' : 
+            return(v1 == v2) ? options.fn(this) :options.inverse(this);
+        case'===' : 
+            return(v1 === v2) ? options.fn(this) :options.inverse(this);
+        case'!=' : 
+            return(v1 != v2) ? options.fn(this) :options.inverse(this);
+        case'!==' : 
+            return(v1 !== v2) ? options.fn(this) :options.inverse(this);
+        case'<' : 
+            return(v1 < v2) ? options.fn(this) :options.inverse(this);
+        case'<=' : 
+            return(v1 <= v2) ? options.fn(this) :options.inverse(this);
+        case'>' : 
+            return(v1 > v2) ? options.fn(this) :options.inverse(this);
+        case'>=' : 
+            return(v1 >= v2) ? options.fn(this) :options.inverse(this);
+        default:
+            return options.inverse(this);
+    }
+});
