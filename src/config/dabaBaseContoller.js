105c8b390c33aa1df7be274304d9ecb46e577cd9
Calzadilla <cg19i04001@usonsonate.edu.sv>
2024-11-01 15:45:04 -0600

    mejora_dbConmtroller

--- src/config/dabaBaseContoller.js
+++ src/config/dabaBaseContoller.js
@@ -8,26 +8,26 @@ const pool = mysql.createPool(database);
 
 // Iniciar conexión con la base de datos
 pool.getConnection((error, conexion) => {
-    // Validar si la conexión tiene algún tipo de error
     if (error) {
         switch (error.code) {
-            case CONSTANTS.PROTOCOL_CONNECTION_LOST: // Notar la corrección en el nombre de la constante
+            case CONSTANTS.PROTOCOL_CONNECTION_LOST:
                 console.error('DATABASE CONNECTION WAS CLOSED');
                 break;
-            case CONSTANTS.ER_CON_COUNT_ERROR: // Demasiadas conexiones
-                console.error('DATABASE HAS TOO MANY CONNECTIONS');
+            case CONSTANTS.ER_CON_COUNT_ERROR:
+                console.error('DATABASE HAS TO MANY CONNECTIONS');
                 break;
-            case CONSTANTS.ECONNREFUSED: // Conexión rechazada (error tipográfico corregido)
-                console.error('DATABASE CONNECTION WAS REFUSED');
+            case CONSTANTS.ECONNREFUSED:
+                console.error('DATABASE CONNECTION WAS CLOSED');
                 break;
-            case CONSTANTS.ER_ACCESS_DENIED_ERROR: // Acceso denegado
-                console.error('ACCESS DENIED FOR USER');
+            case CONSTANTS.ER_ACCESS_DENIED_ERROR:
+                console.error('DATABASE DENIED FOR USER');
                 break;
             default:
-                console.error('UNKNOWN DATABASE ERROR:', error);
+                console.error('ERROR DE BASE DE DATOS NO ENCONTRADO');
                 break;
         }
     }
+
 
     // Si la conexión es exitosa, imprimir mensaje indicando éxito
     if (conexion) {
